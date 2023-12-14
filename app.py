a9ac886751df2e051f4e25ee6a4293ccf5f3b7dc
AgungRizkiNugraha <agungrizki640@gmail.com>
2023-12-14 16:45:14 +0700

    revisi deteksi model

--- app.py
+++ app.py
@@ -1,18 +1,86 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import os
+from flask_mysqldb import MySQL
 from werkzeug.utils import secure_filename
 from tensorflow.keras.models import load_model
 import numpy as np
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from PIL import Image
 from io import BytesIO
+from datetime import datetime, timedelta
+import jwt  # Make sure to import jwt library
+from google.cloud import storage
+import os
+import MySQLdb.cursors
+from dotenv import load_dotenv
 
+
+load_dotenv()
+
+# Membuat payload untuk token
+token_payload = {
+    'exp': datetime.utcnow() + timedelta(minutes=60),
+    'iat': datetime.utcnow()
+}
+
+# Menyandikan token dengan menggunakan kunci rahasia
+encoded_token = jwt.encode(token_payload, os.getenv('JWT_SECRET_KEY'), algorithm='HS256')
+
+print(encoded_token)
 
 app = Flask(__name__)
 CORS(app)
+app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
+jwt = JWTManager(app)
+
+
+# database name
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+
 app.config["ALLOWED_EXTENSIONS"] = set(['png', 'jpg', 'JPEG'])
 app.config['MODEL_FILE'] = "model.h5"
 app.config['LABELS_FILE'] = "label.txt"
+
+
+# Set up Google Cloud Storage
+storage_client = storage.Client.from_service_account_json(os.getenv('GCS_KEY_PATH'))
+bucket_name = os.getenv('GCS_BUCKET_NAME')
+
+@app.route('/')
+def index():
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute('''SELECT * FROM species''')
+    data = cur.fetchall()
+    cur.close() 
+    return jsonify({
+        'status': {
+            'code': 200,
+            'data': data
+        }
+    }), 200
+
+    
+@app.route('/getfoto')
+def indexs():
+    cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute('''SELECT * FROM upload_photo''')
+    data = cur.fetchall()
+    cur.close() 
+    return jsonify({
+        'status': {
+            'code': 200,
+            'data': data
+        }
+    }), 200
+
+
 
 def allowed_file(filename):
     return "." in filename and \
@@ -23,6 +91,7 @@ with open(app.config['LABELS_FILE'], 'r') as file:
     labels = file.read().splitlines()
 
 @app.route("/prediction", methods=["POST"])
+@jwt_required()
 def prediction():
     if 'image' not in request.files:
         return jsonify({
@@ -45,8 +114,9 @@ def prediction():
         }), 400
 
     if image and allowed_file(image.filename):
-        image.seek(0)  
-        img = Image.open(BytesIO(image.read())).convert("RGB")
+        image.seek(0) 
+        image_content = image.read() 
+        img = Image.open(BytesIO(image_content)).convert("RGB")
         img = img.resize((224, 224))
         img_array = np.asarray(img)
         img_array = np.expand_dims(img_array, axis=0)
@@ -56,7 +126,24 @@ def prediction():
         index = np.argmax(prediction)
         class_names = labels[index]
         confidence_score = float(prediction[0][index])
+           
+        
 
+    
+        filename = secure_filename(image.filename)
+        blob = storage_client.bucket(bucket_name).blob(filename)
+        blob.upload_from_string(image_content, content_type=image.content_type)
+
+
+        # Dapatkan URL gambar di Cloud Storage
+        image_url = f"https://storage.googleapis.com/{bucket_name}/{filename}"
+
+
+        cur = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cur.execute('SELECT kingdom, phylum, class, "order", family, species, nama, deskripsi, tingkat_kelangkaan, habitat FROM species WHERE label = % s', (class_names, ))
+        hewan = cur.fetchone()
+        cur.close() 
+   
         return jsonify({
             "status": {
                 "code": 200,
@@ -64,7 +151,9 @@ def prediction():
             },
             "data": {
                 "endangered_prediction": class_names,
-                "confidence": confidence_score
+                "confidence": confidence_score,
+                "image_url": image_url, 
+                "hewan" : hewan
             }
         }), 200
     else:
