01edec684cfb95c7e42acc0bb2a4cb7de746dbbc
AgungRizkiNugraha <agungrizki640@gmail.com>
2023-12-17 02:03:39 +0700

    revisi supaya aman

--- app.py
+++ app.py
@@ -52,7 +52,18 @@ app.config['LABELS_FILE'] = "label.txt"
 
 
 # Set up Google Cloud Storage
-storage_client = storage.Client.from_service_account_json(os.getenv('GCS_KEY_PATH'))
+storage_client = storage.Client.from_service_account_info({
+    "type": os.getenv('TYPE'),
+    "project_id": os.getenv('PROJECT_ID'),
+    "private_key_id": os.getenv('PRIVATE_KEY_ID'),
+    "private_key": os.getenv('PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('CLIENT_EMAIL'),
+    "client_id": os.getenv('CLIENT_ID'),
+    "auth_uri": os.getenv('AUTH_URI'),
+    "token_uri": os.getenv('TOKEN_URI'),
+    "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_X509_CERT_URL'),
+    "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL'),
+})
 bucket_name = os.getenv('GCS_BUCKET_NAME')
 
 @app.route('/')
